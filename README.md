b9ec5d4872c870c3819a59f7a9a6d9c71e3ac832
lefreud <frederic.fortier-chouinard.1@ulaval.ca>
2021-11-11 10:26:37 -0500

    Initial dummy commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AES implementation on AES
+
+This repo should only contain the .VHD files for sources & test benches.
+To use the files, you will need to create your own Vivado project and manually add the sources.
---
+++ sources/MixColumns.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/11/2021 10:26:20 AM
+-- Design Name: 
+-- Module Name: MixColumns - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MixColumns is
+    Port ( a : in STD_LOGIC);
+end MixColumns;
+
+architecture Behavioral of MixColumns is
+
+begin
+
+
+end Behavioral;
