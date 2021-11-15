2a1cf8ab58fb786f2a8bc9b8a4bbabc85610d978
Lucas Niquet <luniq2@ulaval.ca>
2021-11-15 14:50:04 -0500

    Update AES_ROUND.vhd

--- sources/AES_ROUND.vhd
+++ sources/AES_ROUND.vhd
@@ -21,7 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_unsigned.ALL;
+library work;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -32,12 +33,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity AES_ROUND is
---  Port ( );
+  Port (inputText : in STD_LOGIC_VECTOR (127 downto 0);
+        roundKey : in STD_LOGIC_VECTOR (127 downto 0);
+        direction: in STD_LOGIC;
+        outputText : out STD_LOGIC_VECTOR (127 downto 0));
 end AES_ROUND;
 
 architecture Behavioral of AES_ROUND is
+signal subBytes_output: STD_LOGIC_VECTOR (127 downto 0);
+signal shiftRows_output: STD_LOGIC_VECTOR (127 downto 0);
+signal mixColumns_output: STD_LOGIC_VECTOR (127 downto 0);
 
 begin
 
+SubBytes: entity work.SubBytes_128Bits port map(data_in=>inputText, direction=>direction, data_out=>subBytes_output);
+ShiftRows: entity work.ShiftRows port map(data_in=>subBytes_output, direction=>direction, data_out=>shiftRows_output);
+MixColumns: entity work.MixColumns port map(input=>shiftRows_output, direction=>direction, output=>mixColumns_output);
+outputText <= mixColumns_output + roundKey;
 
 end Behavioral;
